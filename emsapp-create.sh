03b736ae26ea48be6d2d25f80850c07e5dd942d3
avish1990 <avish1990@gmail.com>
2018-05-11 12:46:58 +0530

    Update emsapp-create.sh

--- emsapp-create.sh
+++ emsapp-create.sh
@@ -1,7 +1,7 @@
 #gcloud container clusters create <clustername> --network <> --subnetwork <>
 
 #gcloud container clusters get-credentials <clustername> --zone <> --project <projectname>
-
+a
 kubectl apply -f https://raw.githubusercontent.com/avish1990/EMS-DevOps/master/namespace-emsapp.json
 kubectl apply -f https://raw.githubusercontent.com/avish1990/EMS-DevOps/master/configmap-emsapp.yaml
 kubectl apply -f https://raw.githubusercontent.com/avish1990/EMS-DevOps/master/secret-emsapp.yaml
